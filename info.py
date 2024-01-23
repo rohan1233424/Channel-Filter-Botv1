f11d291e4822f54d722453211899d919c6506c4e
rohan1233424 <155885949+rohan1233424@users.noreply.github.com>
2024-01-24 03:08:24 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
-API_ID       = 
-API_HASH     = ""
-BOT_TOKEN    = ""
-SESSION      = ""
-DATABASE_URI = ""
-LOG_CHANNEL  = -1001884373226
-ADMIN        = 1350642629
-CHANNEL      = "@GreyMatter_Bots"
+API_ID       = 27996421
+API_HASH     = "5f5cb6a13ecff3d90e1ec73dc366e26d"
+BOT_TOKEN    = "6567975547:AAEv_03E-6L2FYKqFkN0oPL-JXoYIrPDW8o"
+SESSION      = "BQGrMQUAwRmvJe5c9aS74CYX2-YO1gxy1zJUrnufuhDqhQr5zWiHmvAeKvdXM4c5RAwV1bQyQuLgis62f-EgqrSu4lQjcfiPT1F8kOVXsCdlcx5RMeWkZualF19t1CCocPHJ6sdNOXJUReLUxHT-xyV68oiw9fRwOcLasBHvEA6DTRg5fIukuX2e9t-7UHFTZZz8enqvruBeJrklQ0VZQD_6Wjc-J2Dy87yRCps3nXHQiPj5Tl-7ARfSpeZ9t7ytbzGBCPaJS7LIlTF8Q7EsstJfmlrOLBQnr5GKfEo9Ft1vb7Q11gLbMj_ZiP8u62-dZKcb4mQM5rJp18FsKr0P2JoM2QA-6AAAAAGHe1p7AQ"
+DATABASE_URI = "mongodb+srv://linksearch72:tcTnUrmSQNU0kwlL@cluster0.lysnymj.mongodb.net/?retryWrites=true&w=majority"
+LOG_CHANNEL  = 1993019168
+ADMIN        = 6343583875
+CHANNEL      = "@vegamovies_offical"
